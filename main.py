d6b3b1c97b320a67d8ab98ac88d7f39f2aeb7a31
diaz-81f4 <zaiddirb@gmail.com>
2021-11-18 01:38:16 +0300

    added start screen

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ A program that can measure words per minute when typing phrases
 """
 
 def start_screen():
+    """
+    Start screen text for user
+    """
+    print("------[Welcome to WPM type tester!]------")
+    print()
+    print(" - Text will be displayed")
+    print(" - whenever ready type r click enter and a timer will start")
+
+
+def display_phrases():
+    """
+    display text to user
+    """
     pass
 
-def display_text():
-    pass
-
-def load_phrases():
+def load_phrase():
     """
     Reads phrases from a file called phrases.txt for user to write in terminal to practice
     """
@@ -17,13 +27,14 @@ def load_phrases():
     lines = f.readlines()
     for line in lines:
         all_phrases += line
-    print(all_phrases)
+    
 
 def wpm():
     pass
 
 def main():
-    load_phrases()
+    start_screen()
+    # load_phrases()
 
 if __name__ == '__main__':
     main()
